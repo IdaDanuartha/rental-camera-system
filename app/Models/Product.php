077c718113dc733bf1a4e0a5b15e5c11082c2e7d
IdaDanuartha <danuart21@gmail.com>
2024-01-27 22:11:55 +0800

    update product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function deviceSeries(): BelongsTo
+    {
+        return $this->belongsTo(DeviceSeries::class);
+    }
+
+    public function productImages(): HasMany
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function bookingDetail(): HasMany
+    {
+        return $this->hasMany(BookingDetail::class);
+    }
 }
