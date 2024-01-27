94aaba0a4c4b092e07df95bad75c83f572d107c0
IdaDanuartha <danuart21@gmail.com>
2024-01-27 22:07:03 +0800

    update booking facility detail model

--- app/Models/BookingFacilityDetail.php
+++ app/Models/BookingFacilityDetail.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BookingFacilityDetail extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function bookingFacility(): BelongsTo
+    {
+        return $this->belongsTo(BookingFacility::class);
+    }
+
+    public function facility(): BelongsTo
+    {
+        return $this->belongsTo(Facility::class);
+    }
 }
