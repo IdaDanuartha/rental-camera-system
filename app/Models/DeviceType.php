16014ff59e67bcba4b479cbe367192489f060fdf
IdaDanuartha <danuart21@gmail.com>
2024-01-27 22:08:46 +0800

    update device type model

--- app/Models/DeviceType.php
+++ app/Models/DeviceType.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class DeviceType extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function deviceBrands(): HasMany
+    {
+        return $this->hasMany(DeviceBrand::class);
+    }
 }
