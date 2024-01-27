30c0eaec3e87c7e62bca1a7848117ae1a0541227
IdaDanuartha <danuart21@gmail.com>
2024-01-27 22:08:40 +0800

    update device series model

--- app/Models/DeviceSeries.php
+++ app/Models/DeviceSeries.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class DeviceSeries extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function deviceBrand(): BelongsTo
+    {
+        return $this->belongsTo(DeviceBrand::class);
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
