656f445f31651b21ad85be1972ddbc440c504ff7
IdaDanuartha <danuart21@gmail.com>
2024-01-27 22:11:48 +0800

    update facility type model

--- app/Models/FacilityType.php
+++ app/Models/FacilityType.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class FacilityType extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function facilities(): HasMany
+    {
+        return $this->hasMany(Facility::class);
+    }
 }
