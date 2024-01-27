c7cffe87ee4496951a116a8079f6f17015768da8
IdaDanuartha <danuart21@gmail.com>
2024-01-27 22:07:56 +0800

    update device brand model

--- app/Models/DeviceBrand.php
+++ app/Models/DeviceBrand.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class DeviceBrand extends Model
 {
     use HasFactory;
+    
+    protected $guarded = ['id'];
+
+    public function deviceType(): BelongsTo
+    {
+        return $this->belongsTo(deviceType::class);
+    }
+
+    public function deviceSeries(): HasMany
+    {
+        return $this->hasMany(DeviceSeries::class);
+    }
 }
