9c2c11ec571eb4db662a8ec502cf63b86235e12a
IdaDanuartha <danuart21@gmail.com>
2024-01-27 22:06:27 +0800

    update booking model

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Booking extends Model
 {
     use HasFactory;
+    
+    protected $guarded = ['id'];
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function staff(): BelongsTo
+    {
+        return $this->belongsTo(Staff::class);
+    }
+
+    public function bookingDetails(): HasMany
+    {
+        return $this->hasMany(BookingDetail::class);
+    }
 }
