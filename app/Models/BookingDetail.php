76bf1b6c62baa099e2fc518aecf945cc5299a258
IdaDanuartha <danuart21@gmail.com>
2024-01-27 22:06:35 +0800

    update booking detail model

--- app/Models/BookingDetail.php
+++ app/Models/BookingDetail.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BookingDetail extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function booking(): BelongsTo
+    {
+        return $this->belongsTo(Booking::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
