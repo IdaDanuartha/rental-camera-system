7750b12a8ebed75249c87f3283d17aa7b98fd4d9
IdaDanuartha <danuart21@gmail.com>
2024-01-28 13:46:13 +0800

    update model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = ['id'];
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for serialization.
