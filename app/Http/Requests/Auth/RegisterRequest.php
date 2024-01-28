05adf94196e5567d624fa1937ee3de46860e9a74
IdaDanuartha <danuart21@gmail.com>
2024-01-28 13:46:08 +0800

    add register request

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "username" => "required|alpha_dash|min:5",
+            "email" => "required|email:dns|unique:users,email",
+            "password" => "required|same:confirm_password|min:6"
+        ];
+    }
+}
