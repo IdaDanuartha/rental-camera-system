2773e4013fcc8db1cef991a65a5fd097f420142a
IdaDanuartha <danuart21@gmail.com>
2024-01-28 22:31:52 +0800

    update dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,6 @@ class DashboardController extends Controller
 {
     public function __invoke()
     {
-        return view('admin.dashboard.index');
+        return view('dashboard.analytics.index');
     }
 }
