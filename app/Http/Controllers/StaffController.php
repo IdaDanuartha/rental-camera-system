e27d7067bc350b947ef71fae9029edb8078abb7f
IdaDanuartha <danuart21@gmail.com>
2024-01-28 22:32:10 +0800

    update staff controller

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -2,63 +2,78 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Staff\StoreStaffRequest;
+use App\Http\Requests\Staff\UpdateStaffRequest;
+use App\Models\Staff;
+use App\Repositories\StaffRepository;
+use App\Utils\ResponseMessage;
+use Exception;
 use Illuminate\Http\Request;
 
 class StaffController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+    public function __construct(
+        protected readonly StaffRepository $staff,
+        protected readonly ResponseMessage $responseMessage
+    ) {}
+
+    public function index(Request $request)
+    {                                   
+        $staff = $this->staff->findAllPaginate();
+        return view('dashboard.staff.index', compact('staff'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
-    {
-        //
+    {                                           
+        return view('dashboard.staff.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+    public function show(Staff $staff)
+    {                                                   
+        return view('dashboard.staff.detail', compact('staff'));
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+    public function edit(Staff $staff)
+    {                                           
+        return view('dashboard.staff.edit', compact('staff'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+    public function store(StoreStaffRequest $request)
+    {        
+        try {
+            $store = $this->staff->store($request->validated());
+
+            if($store) return redirect(route("staff.index"))
+                                ->with("success", $this->responseMessage->response('Staff'));
+            throw new Exception;
+        } catch (\Exception $e) {  
+            logger($e->getMessage());
+
+            return redirect(route("staff.create"))->with("failed", $this->responseMessage->response('staff', false));
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(UpdateStaffRequest $request, Staff $staff)
     {
-        //
+        try {                     
+            $update = $this->staff->update($request->validated(), $staff);
+
+            if($update) return redirect(route('staff.index'))
+                                ->with('success', $this->responseMessage->response('Staff', true, 'update'));
+            throw new Exception;
+        } catch (\Exception $e) {
+            return redirect()->route('staff.edit', $staff->id)->with('error', $this->responseMessage->response('staff', false, 'update'));
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy(Staff $staff)
     {
-        //
+        try {
+            $this->staff->delete($staff);
+
+            return redirect()->route('staff.index')->with('success', $this->responseMessage->response('Staff', true, 'delete'));
+        } catch (\Exception $e) {            
+            return redirect()->route('staff.index')->with('error', $this->responseMessage->response('staff', false, 'delete'));
+        }
     }
 }
