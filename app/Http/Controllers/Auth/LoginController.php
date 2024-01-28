65aff863e077dd19d24a756d3895a111065fab21
IdaDanuartha <danuart21@gmail.com>
2024-01-28 13:45:27 +0800

    update login controller

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Repositories\AuthRepository;
+
+class LoginController extends Controller
+{
+    public function __construct(
+        private readonly AuthRepository $authRepository
+    ) {}
+
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate(LoginRequest $request)
+    {
+        try {
+            $this->authRepository->login($request->only("email", "password"));
+
+            return redirect()->route("dashboard.index")->with('success', 'Login success! Welcome back ' . auth()->user()->username);                    
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Login failed! Check your credentials and try again');
+        }
+    }
+}
