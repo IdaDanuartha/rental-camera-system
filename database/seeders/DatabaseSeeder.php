78256acb8fb22ec13d695f7e614538eb86291e6f
IdaDanuartha <danuart21@gmail.com>
2024-01-31 12:54:54 +0800

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,9 @@ namespace Database\Seeders;
 
 use App\Enums\Role;
 use App\Models\Admin;
+use App\Models\DeviceBrand;
+use App\Models\DeviceSeries;
+use App\Models\DeviceType;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,5 +24,17 @@ class DatabaseSeeder extends Seeder
             'password' => 'admin',
             'role' => Role::ADMIN
         ]);
+
+        DeviceType::create(['name' => 'Mirrorless']);
+        DeviceType::create(['name' => 'DSLR']);
+        DeviceType::create(['name' => 'Action Camera']);
+
+        DeviceBrand::create(['device_type_id' => 1, 'name' => 'Sony']);
+        DeviceBrand::create(['device_type_id' => 2, 'name' => 'Canon']);
+        DeviceBrand::create(['device_type_id' => 3, 'name' => 'Fuji Film']);
+        
+        DeviceSeries::create(['device_brand_id' => 1, 'name' => 'Sony A5000']);
+        DeviceSeries::create(['device_brand_id' => 2, 'name' => 'Canon 600D']);
+        DeviceSeries::create(['device_brand_id' => 2, 'name' => 'Canon 60D']);
     }
 }
