1e458aa7ee3190e252accc1258341570b7bb0e80
Ida Danuartha <danuart21@gmail.com>
2024-02-03 23:27:13 +0800

    update code product repository

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -5,6 +5,7 @@ namespace App\Repositories;
 use App\Enums\Role;
 use App\Enums\UserStatus;
 use App\Models\Product;
+use App\Models\ProductImage;
 use App\Utils\UploadFile;
 use Exception;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
@@ -16,29 +17,44 @@ class ProductRepository
 {
   public function __construct(
     protected readonly Product $product,
+    protected readonly ProductImage $productImage,
     protected readonly UploadFile $uploadFile
   ) {}
 
   public function findAll(): Collection
   {
-    return $this->product->latest()->with(['deviceSeries.deviceBrand'])->get();
+    return $this->product->latest()->with(['deviceSeries.deviceBrand', 'productImages'])->get();
   }
 
   public function findAllPaginate(): LengthAwarePaginator
   {
-    return $this->product->latest()->with(['deviceSeries.deviceBrand'])->paginate(10);
+    return $this->product->latest()->with(['deviceSeries.deviceBrand', 'productImages'])->paginate(10);
   }
 
   public function findById(Product $product): Product
   {
-    return $this->product->where('id', $product->id)->with(['deviceSeries.deviceBrand'])->first();
+    return $this->product->where('id', $product->id)->with(['deviceSeries.deviceBrand', 'productImages'])->first();
+  }
+
+  public function findImageDeleted($product_id, $image_deleted): Collection
+  {
+    return $this->productImage->where('product_id', $product_id)->whereIn("id", explode(",", $image_deleted["image_deleted"]))->get(); 
   }
 
   public function store($request): Product|Exception
   {
     DB::beginTransaction();
-    try {  
-      $product = $this->product->create($request);
+    try {   
+      $product = $this->product->create(Arr::except($request, "images"));
+      
+      foreach($request["images"] as $image) {
+        $filename = $this->uploadFile->uploadSingleFile($image, "products");
+        $this->productImage->create([
+          "product_id" => $product->id,
+          "image" => $filename
+        ]);
+      }
+
     } catch (\Exception $e) {  
       logger($e->getMessage());
       DB::rollBack();
@@ -53,7 +69,22 @@ class ProductRepository
   {
     DB::beginTransaction();    
     try {  
-      $product->updateOrFail($request);
+      if(Arr::get($request, "image_deleted")) {
+        foreach($this->findImageDeleted($product->id, Arr::only($request, "image_deleted")) as $image) {                      
+          $this->uploadFile->deleteExistFile("products/$image->image");        
+          $image->delete();
+        } 
+      }
+      
+      foreach($request["images"] as $image) {
+        $filename = $this->uploadFile->uploadSingleFile($image, "products");
+        $this->productImage->create([
+          'product_id' => $product->id,
+          'image' => $filename
+        ]);
+      }
+
+      $product_updated = $product->updateOrFail($request);
     } catch (\Exception $e) {  
       logger($e->getMessage());
       DB::rollBack();
@@ -62,13 +93,18 @@ class ProductRepository
     }
 
     DB::commit();
-    return true;
+    return $product_updated;
   }
 
   public function delete(Product $product): bool
   {
     DB::beginTransaction();
     try {
+      foreach($product->productImages as $image) {                              
+        $this->uploadFile->deleteExistFile("products/$image->image");
+        $image->delete();
+      } 
+
       $delete_product = $product->deleteOrFail();
     } catch (\Exception $e) {
       logger($e->getMessage());
