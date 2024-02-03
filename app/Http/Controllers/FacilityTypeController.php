1aea6ffb7b16cc91bc55fb8e2b9c12e08fca4750
Ida Danuartha <danuart21@gmail.com>
2024-02-03 21:28:31 +0800

    update facility type controller

--- app/Http/Controllers/FacilityTypeController.php
+++ app/Http/Controllers/FacilityTypeController.php
@@ -2,63 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\FacilityType\StoreFacilityTypeRequest;
+use App\Http\Requests\FacilityType\UpdateFacilityTypeRequest;
+use App\Models\FacilityType;
+use App\Repositories\FacilityTypeRepository;
+use App\Utils\ResponseMessage;
+use Exception;
 use Illuminate\Http\Request;
 
 class FacilityTypeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+    public function __construct(
+        protected readonly FacilityTypeRepository $deviceType,
+        protected readonly ResponseMessage $responseMessage
+    ) {}
+
+    public function index(Request $request)
+    {                                   
+        $facility_types = $this->deviceType->findAllPaginate();
+        return view('dashboard.facility-types.index', compact('facility_types'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    public function show(FacilityType $type)
+    {                                                   
+        return response()->json([
+            "facility_type" => $this->deviceType->findById($type)
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+    public function store(StoreFacilityTypeRequest $request)
+    {        
+        try {
+            $store = $this->deviceType->store($request->validated());
+
+            if($store) return redirect(route("facilities.types.index"))
+                                ->with("success", $this->responseMessage->response('Facility type'));
+            throw new Exception;
+        } catch (\Exception $e) {  
+            logger($e->getMessage());
+
+            return redirect(route("facilities.types.index"))->with("error", $this->responseMessage->response('facility type', false));
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function update(UpdateFacilityTypeRequest $request, FacilityType $type)
     {
-        //
+        try {                     
+            $update = $this->deviceType->update($request->validated(), $type);
+
+            if($update) return redirect(route('facilities.types.index'))
+                                ->with('success', $this->responseMessage->response('Facility type', true, 'update'));
+            throw new Exception;
+        } catch (\Exception $e) {
+            return redirect()->route('facilities.types.index', $type->id)->with('error', $this->responseMessage->response('facility type', false, 'update'));
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function destroy(FacilityType $type)
     {
-        //
-    }
+        try {
+            $this->deviceType->delete($type);
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+            return redirect()->route('facilities.types.index')->with('success', $this->responseMessage->response('Facility type', true, 'delete'));
+        } catch (\Exception $e) {            
+            return redirect()->route('facilities.types.index')->with('error', $this->responseMessage->response('facility type', false, 'delete'));
+        }
     }
 }
