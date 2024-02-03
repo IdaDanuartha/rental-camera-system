66ba1435052d75ef8507948e40d6f0f3459da8b5
Ida Danuartha <danuart21@gmail.com>
2024-02-03 23:27:05 +0800

    update code facility repository

--- app/Repositories/FacilityRepository.php
+++ app/Repositories/FacilityRepository.php
@@ -5,6 +5,7 @@ namespace App\Repositories;
 use App\Enums\Role;
 use App\Enums\UserStatus;
 use App\Models\Facility;
+use App\Models\FacilityImage;
 use App\Models\Product;
 use App\Utils\UploadFile;
 use Exception;
@@ -17,29 +18,44 @@ class FacilityRepository
 {
   public function __construct(
     protected readonly Facility $facility,
+    protected readonly FacilityImage $facilityImage,
     protected readonly UploadFile $uploadFile
   ) {}
 
   public function findAll(): Collection
   {
-    return $this->facility->latest()->with(['facilityType'])->get();
+    return $this->facility->latest()->with(['facilityType', 'facilityImages'])->get();
   }
 
   public function findAllPaginate(): LengthAwarePaginator
   {
-    return $this->facility->latest()->with(['facilityType'])->paginate(10);
+    return $this->facility->latest()->with(['facilityType', 'facilityImages'])->paginate(10);
   }
 
   public function findById(Facility $facility): Facility
   {
-    return $this->facility->where('id', $facility->id)->with(['facilityType'])->first();
+    return $this->facility->where('id', $facility->id)->with(['facilityType', 'facilityImages'])->first();
+  }
+
+  public function findImageDeleted($facility_id, $image_deleted): Collection
+  {
+    return $this->facilityImage->where('facility_id', $facility_id)->whereIn("id", explode(",", $image_deleted["image_deleted"]))->get(); 
   }
 
   public function store($request): Facility|Exception
   {
     DB::beginTransaction();
-    try {  
-      $product = $this->facility->create($request);
+    try {   
+      $facility = $this->facility->create(Arr::except($request, "images"));
+      
+      foreach($request["images"] as $image) {
+        $filename = $this->uploadFile->uploadSingleFile($image, "facilities");
+        $this->facilityImage->create([
+          "facility_id" => $facility->id,
+          "image" => $filename
+        ]);
+      }
+
     } catch (\Exception $e) {  
       logger($e->getMessage());
       DB::rollBack();
@@ -47,14 +63,29 @@ class FacilityRepository
       return $e;
     }
     DB::commit();
-    return $product;
+    return $facility;
   }
 
   public function update($request, Facility $facility): bool
   {
     DB::beginTransaction();    
     try {  
-      $facility->updateOrFail($request);
+      if(Arr::get($request, "image_deleted")) {
+        foreach($this->findImageDeleted($facility->id, Arr::only($request, "image_deleted")) as $image) {                      
+          $this->uploadFile->deleteExistFile("facilities/$image->image");        
+          $image->delete();
+        } 
+      }
+      
+      foreach($request["images"] as $image) {
+        $filename = $this->uploadFile->uploadSingleFile($image, "facilities");
+        $this->facilityImage->create([
+          'facility_id' => $facility->id,
+          'image' => $filename
+        ]);
+      }
+
+      $facility_updated = $facility->updateOrFail($request);
     } catch (\Exception $e) {  
       logger($e->getMessage());
       DB::rollBack();
@@ -63,14 +94,19 @@ class FacilityRepository
     }
 
     DB::commit();
-    return true;
+    return $facility_updated;
   }
 
   public function delete(Facility $facility): bool
   {
     DB::beginTransaction();
     try {
-      $delete_product = $facility->deleteOrFail();
+      foreach($facility->facilityImages as $image) {                              
+        $this->uploadFile->deleteExistFile("facilities/$image->image");
+        $image->delete();
+      } 
+
+      $delete_facility = $facility->deleteOrFail();
     } catch (\Exception $e) {
       logger($e->getMessage());
       DB::rollBack();
@@ -79,6 +115,6 @@ class FacilityRepository
     }    
 
     DB::commit();
-    return $delete_product;
+    return $delete_facility;
   }
 }
